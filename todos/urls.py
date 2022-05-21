69d5aab87b08c88012558c622ea57356745f784f
kaveh moradian <kave.moradian@gmail.com>
2022-05-21 19:56:59 +0430

    create urls endpoints

---
+++ todos/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+app_name = 'todo'
+
+urlpatterns = [
+    #path('', api_docs, name='api_docs'),
+    #path('tasks/', tasks_list, name='tasks_list'),
+    #path('task-detail/<pk>/', tasks_detail, name='tasks_detail'),
+    #path('add-task/', new_task, name="new_task"),
+    #path('edit-task/<pk>/', edit_task, name="edit_task"),
+    #path('delete-task/<pk>/', delete_task, name="delete_task"),
+]
