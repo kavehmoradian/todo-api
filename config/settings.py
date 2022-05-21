c7a59930a1b7a946a84fa2e79667390551e8d024
kaveh moradian <kave.moradian@gmail.com>
2022-05-21 19:26:40 +0430

    create todo app

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party
+    'rest_framework',
+    #local
+    'todos',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +82,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 
 
